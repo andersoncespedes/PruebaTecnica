8ff3ad9b2485c731cc4f467809303924d5acd7b3
anderson <andersontrabajos2@gmail.com>
2024-03-10 11:50:50 -0500

    perf: :loud_sound: Creacion de logs

--- API/Controllers/FlightController.cs
+++ API/Controllers/FlightController.cs
@@ -7,14 +7,19 @@ namespace API.Controllers;
 public class FlightController : BaseApiController
 {
     private readonly IAPIGetter _api;
-    public FlightController(IAPIGetter api){
+    private readonly ILogger<FlightController> _logger;
+    public FlightController(IAPIGetter api, ILogger<FlightController> logger){
         _api = api;
+        _logger = logger;
     }
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IEnumerable<FlightDto>> Get(){
-        return await _api.GetOrSetCacheApi();
+        _logger.LogInformation("Obteniendo los vuelos....");
+        IEnumerable<FlightDto> api = await _api.GetOrSetCacheApi();
+        _logger.LogInformation("Vuelos Encontrados");
+        return api;
     }
 
 }
--- API/Controllers/JourneyController.cs
+++ API/Controllers/JourneyController.cs
@@ -13,11 +13,13 @@ public class JourneyController : BaseApiController
     private readonly IUnitOfWork _unitOfWork;
     private readonly IAPIGetter _api;
     private readonly IMapper _mapper;
-    public JourneyController(IUnitOfWork unitOfWork, IAPIGetter api, IMapper mapper)
+    private readonly ILogger<JourneyController> _logger;
+    public JourneyController(IUnitOfWork unitOfWork, IAPIGetter api, IMapper mapper, ILogger<JourneyController> logger)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
         _api = api;
+        _logger = logger;
     }
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
@@ -30,22 +32,28 @@ public class JourneyController : BaseApiController
             entity.Origin = entity.Origin.ToUpper();
             entity.Destination = entity.Destination.ToUpper();
             // Verificamos si existe en la base de datos un viaje que coincida con el pasado por parametro
+            _logger.LogInformation("Buscando Ruta en la base de datos...");
             Journey journey = _unitOfWork.Journies
                 .Find(e => e.Origin == entity.Origin && e.Destination == entity.Destination)
                 .FirstOrDefault();
                 // verificamos si el resultado dio null
             if (journey != null)
             {
+                _logger.LogInformation("Ruta Encontrada en la Base de Datos");
                 // si dio diferente a null entonces mapeamos la respuesta con el JourneyDto y la retornamos 
                 return _mapper.Map<JourneyDto>(journey);
             }
             else
             {
+                _logger.LogWarning("Ruta No Encontrada en la base de datos");
                 // si dio null entonces utilizamos el servicio api y llamamos el metodo GetJourney
+                _logger.LogInformation("Obteniendo ruta por la API...");
                 JourneyDto journeydto = await _api.GetJourney(entity);
                 // Mapeamos el resulado como la entidad Journey
+                _logger.LogInformation("Mapeando Resultados...");
                 journey = _mapper.Map<Journey>(journeydto);
                 // Creamos un nuevo viaje en la base de datos
+                _logger.LogInformation("Guardando Resultados...");
                 _unitOfWork.Journies.Create(journey);
                 // Guardamos los cambios
                 await _unitOfWork.SaveChangesAsync();
@@ -56,14 +64,17 @@ public class JourneyController : BaseApiController
         // Control de excepciones
         catch (InvalidOperationException err)
         {
+            _logger.LogError("NO SE ENCONTRO LA RUTA");
             return BadRequest(err.Message);
         }
         catch (ArgumentNullException)
         {
+            _logger.LogError("NO SE ENCONTRO DESTINO O ORIGEN");
             return NotFound("No se encontro la ubicacion");
         }
         catch (Exception)
         {
+            _logger.LogError("ERROR DESCONOCIDO");
             return Ok("No Se pudo realizar la operacion");
         }
     }
